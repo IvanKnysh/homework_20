c1e144066b014fb7f23eb73aeeac7f156a693fbc
Ivan Knysh <mr.rauber.ik@gmail.com>
2023-11-10 16:23:40 +0200

    Added login for step 2

--- js/app.js
+++ js/app.js
@@ -3,42 +3,69 @@ const step_2 = document.querySelector(".step-2");
 const step_3 = document.querySelector(".step-3");
 const step_1_button = document.querySelector(".step-1 button");
 const step_2_button = document.querySelector(".step-2 button");
-const step_2_getAllInputs = document.querySelectorAll(".step-2 input");
-
-// Navigation by screens
-step_1_button.addEventListener("click", () => {
-	step_1.classList.add("disabled");
-	step_2.classList.add("active");
-});
 
 // Check inputs by input
-step_2_getAllInputs.forEach((item) => {
-	item.addEventListener("input", () => {
-		if (item.value.trim() == "") {
-			item.classList.add("error");
-		} else {
-			item.classList.remove("error");
-		}
+const checkInputsByInput = (getAllInputs) => {
+	getAllInputs.forEach((item) => {
+		item.addEventListener("input", () => {
+			if (item.value.trim() == "") {
+				item.classList.add("error");
+			} else {
+				item.classList.remove("error");
+			}
+		});
 	});
-});
+};
 
 // Check inputs by click
-step_2_button.addEventListener("click", () => {
-	const arr = [];
+const checkInputsByClick = (getAllInputs) => {
+	step_2_button.addEventListener("click", () => {
+		const arr = [];
 
-	step_2_getAllInputs.forEach((item) => {
-		if (item.value.trim() == "") {
-			item.classList.add("error");
-			arr.push(1);
-		} else {
-			arr.push(0);
+		getAllInputs.forEach((item) => {
+			if (item.value.trim() == "") {
+				item.classList.add("error");
+				arr.push(1);
+			} else {
+				arr.push(0);
+			}
+		});
+
+		const isValidate = arr.every((item) => item === 0);
+
+		if (isValidate) {
+			step_2.classList.remove("active");
+			step_3.classList.add("active");
 		}
 	});
+};
 
-	const isValidate = arr.every((item) => item === 0);
+// Generate inputs for step 2
+step_1_button.addEventListener("click", () => {
+	const participantsValue = +document.querySelector("#participants").value;
+	const countriesValue = +document.querySelector("#countries").value;
 
-	if (isValidate) {
-		step_2.classList.remove("active");
-		step_3.classList.add("active");
+	for (let i = 0; i < participantsValue; i++) {
+		step_2
+			.querySelector(".block:nth-child(1)")
+			.insertAdjacentHTML(
+				"beforeend",
+				`<input type="text" placeholder="Учасник ${i + 1}" />`
+			);
 	}
+
+	for (let i = 0; i < countriesValue; i++) {
+		step_2
+			.querySelector(".block:nth-child(2)")
+			.insertAdjacentHTML(
+				"beforeend",
+				`<input type="text" placeholder="Країна ${i + 1}" />`
+			);
+	}
+
+	checkInputsByInput(document.querySelectorAll(".step-2 input"));
+	checkInputsByClick(document.querySelectorAll(".step-2 input"));
+
+	step_1.classList.add("disabled");
+	step_2.classList.add("active");
 });
